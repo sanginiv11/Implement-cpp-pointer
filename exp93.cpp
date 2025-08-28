d7c0670d950e939405f6e3179afea63a5f213f42
Sangini Verma <sanginiv11@gmail.com>
2025-08-28 14:36:10 +0530

    Add files via upload

---
+++ exp93.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+ int main() {
+    int a = 10;
+    int *aptr = &a;
+
+    cout<<*aptr<<endl;
+    *aptr=20;
+    cout<<a<<endl;
+
+    int arr[]= {10,20,30};
+    cout<<arr<<endl;
+
+    return 0;
+
+ }
---
+++ exp95.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+void increment(int a){
+    a++;
+}
+int main() {
+    int a = 2;
+    increment(a);
+    cout<<a<< endl;
+
+return 0;    
+}    
